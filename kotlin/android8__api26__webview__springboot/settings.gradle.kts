6331274c0bf3e346be83647ae0b92e0dbf75434d
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2025-09-18 23:48:19 +0800

    feat webview

---
+++ kotlin/android8__api26__webview__springboot/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android8__api26__webview__springboot"
+include(":app")
