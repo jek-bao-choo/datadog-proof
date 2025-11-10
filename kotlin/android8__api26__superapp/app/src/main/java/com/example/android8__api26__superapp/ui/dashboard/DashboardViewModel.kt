9096de91d3c6369db63a9877ec72f93aa80c72cc
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2025-11-10 11:51:23 +0800

    kotlin init super app

---
+++ kotlin/android8__api26__superapp/app/src/main/java/com/example/android8__api26__superapp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.android8__api26__superapp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
