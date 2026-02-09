6316d95d80450e21fe9d5514569fdbcbf91edd2c
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2026-02-09 15:48:21 +0800

    Update traffic.sh

--- java/springboot4dot0dot2__openjdk21__payload2customtags/traffic.sh
+++ java/springboot4dot0dot2__openjdk21__payload2customtags/traffic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ERROR_CODES=("AC01" "FF02" "AC03" "FF10" "DS04" "DS24")
-SWIFT_CODES=("OCBCSGSGXXX" "BNORPHMMXXX" "DBSSSGSGXXX" "PNBMPHMMXXX")
+SWIFT_CODES=("OCBCSGSGXXX" "BNORPHMMXXX" "DBSSSGSGXXX" "PNBMPHMMXXX" "WUFSUS55XXX" "EBILAEADXXX")
 DIMENSIONS=("creditOtherBank" "creditCASAOnLine" "pickUpCash" "checkStatus")
 
 while true; do
@@ -10,7 +10,7 @@ while true; do
   dimension=${DIMENSIONS[$RANDOM % ${#DIMENSIONS[@]}]}
 
   echo "Sending: customErrorCode=$error, customBankCode=$swift, customDimensionCode=$dimension"
-  curl -s -X GET http://localhost:8080/payload-to-spantags \
+  curl -s -X GET http://3.1.102.190:8080/payload-to-spantags \
     -H "Content-Type: application/json" \
     -d "{\"customErrorCode\":\"$error\", \"customBankCode\":\"$swift\", \"customDimensionCode\":\"$dimension\"}"
   echo ""
