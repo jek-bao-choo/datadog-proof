a2a9e4dee4881d423d5b86deb9122559a755cee5
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2026-02-10 17:39:41 +0800

    update

--- dockerfile/otelcol/send_trace.py
+++ dockerfile/otelcol/send_trace.py
@@ -8,12 +8,12 @@ from opentelemetry.sdk.resources import Resource
 resource = Resource.create({"service.name": "jek-svc-v6"})
 provider = TracerProvider(resource=resource)
 exporter = OTLPSpanExporter(
-    endpoint="http://localhost:4318/v1/traces",
-    # endpoint="https://otlp.datadoghq.com/v1/traces", # Enable this headers only if I want to send to Datadog direct
-    # headers={ # Enable this headers only if I want to send to Datadog direct
-    #     "dd-api-key": os.getenv("DD_API_KEY"),
-    #     "dd-otlp-source": "datadog"
-    # }
+    # endpoint="http://localhost:4318/v1/traces",
+    endpoint="https://otlp.datadoghq.com/v1/traces", # Enable this headers only if I want to send to Datadog direct
+    headers={ # Enable this headers only if I want to send to Datadog direct
+        "dd-api-key": os.getenv("DD_API_KEY"),
+        "dd-otlp-source": "datadog"
+    }
 )
 provider.add_span_processor(BatchSpanProcessor(exporter))
 trace.set_tracer_provider(provider)
