bb8c82b0dc7079cc47932ad73d7d2720a68f48f9
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2026-02-10 16:10:34 +0800

    update

--- dockerfile/otelcol/send_trace.py
+++ dockerfile/otelcol/send_trace.py
@@ -1,19 +1,18 @@
-# import os
+import os
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 
-resource = Resource.create({"service.name": "jek-svc-v1"})
+resource = Resource.create({"service.name": "jek-svc-v6"})
 provider = TracerProvider(resource=resource)
 exporter = OTLPSpanExporter(
     endpoint="http://localhost:4318/v1/traces",
     # endpoint="https://otlp.datadoghq.com/v1/traces", # Enable this headers only if I want to send to Datadog direct
     # headers={ # Enable this headers only if I want to send to Datadog direct
     #     "dd-api-key": os.getenv("DD_API_KEY"),
-    #     "dd-otel-span-mapping": "{span_name_as_resource_name: true}", # Specifies whether to use the OpenTelemetry span’s name as the Datadog span’s operation name (default: true). If false, the operation name is derived from a combination of the instrumentation scope name and span kind.
-    #     "dd-otlp-source": "US1" # Replace with the specific value provided by Datadog for your organization
+    #     "dd-otlp-source": "datadog"
     # }
 )
 provider.add_span_processor(BatchSpanProcessor(exporter))
