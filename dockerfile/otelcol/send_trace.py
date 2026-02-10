37d30926e9c604eb5b3ac6e44b97144142438945
Jek Bao CHOO <6467136+jek-bao-choo@users.noreply.github.com>
2026-02-10 11:43:33 +0800

    Update send_trace.py

--- dockerfile/otelcol/send_trace.py
+++ dockerfile/otelcol/send_trace.py
@@ -1,3 +1,4 @@
+# import os
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -10,7 +11,7 @@ exporter = OTLPSpanExporter(
     endpoint="http://localhost:4318/v1/traces",
     # endpoint="https://otlp.datadoghq.com/v1/traces", # Enable this headers only if I want to send to Datadog direct
     # headers={ # Enable this headers only if I want to send to Datadog direct
-    #     "dd-api-key": "<REPLACE_WITH_DATADOG_API_KEY>",
+    #     "dd-api-key": os.getenv("DD_API_KEY"),
     #     "dd-otel-span-mapping": "{span_name_as_resource_name: true}", # Specifies whether to use the OpenTelemetry span’s name as the Datadog span’s operation name (default: true). If false, the operation name is derived from a combination of the instrumentation scope name and span kind.
     #     "dd-otlp-source": "US1" # Replace with the specific value provided by Datadog for your organization
     # }
